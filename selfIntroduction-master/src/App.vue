dff10d618fcc39e553038a31380451d3d4447e83
Creation Is Fat <freestyle.baller.sk8er@gmail.com>
2020-08-15 10:31:13 +0900

    ddd

---
+++ selfIntroduction-master/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/aiueo">aiueo</router-link> |
+      <router-link to="/selfIntroduction">self-introduction</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  height: 100vh;
+}
+
+#nav {
+  padding: 30px;
+  background-color: black;
+
+  a {
+    font-weight: bold;
+    color: #c5c909;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ selfIntroduction-master/src/views/selfIntroduction.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="row pearent">
+        <div class="col-4 h-75 topicArea">
+            <img alt="Rohan" src="../assets/kishiberohan.png" height="300px">
+        </div>
+        <div class="col-8 h-75 explainArea">
+            <SotahExplainArea class="h-100"/>
+            <div class="d-none">
+                ExplainArea
+                <p>{{ greatText }}</p>
+                <p>
+                    <SotahButton :great="greatText" @click="onSotahButtonClicked" />
+                </p>
+                <p>
+                    <SotahResetButton :initialValue="Hello" v-model="greatText" />
+                </p>
+            </div>
+        </div>
+        <div class="col footerArea h-25">
+            footerArea
+        </div>
+    </div>
+</template>
+
+<script lang=ts>
+import { Component, Vue } from "vue-property-decorator";
+import SotahButton from "@/components/SotahButton.vue";
+import { component } from 'vue/types/umd';
+import SotahResetButton from "@/components/SotahResetButton.vue";
+import SotahExplainArea from "@/components/organisms/SotahExplainArea.vue";
+
+@Component({
+    components: {
+        SotahButton,
+        SotahResetButton,
+        SotahExplainArea,
+    },
+})
+export default class selfIntroduction extends Vue {
+    public greatText: string = "Hello";
+
+    public onSotahButtonClicked(){
+        this.greatText = "こんにちは";
+    }
+}
+
+</script>
+
+<style lang="scss">
+.pearent {
+    height:100%;
+}
+.topicArea {
+    background-color: aqua;
+}
+.explainArea {
+    background-color:blanchedalmond;
+}
+.footerArea {
+    background-color: rgb(8, 8, 8);
+    color: chartreuse;
+}
+</style>
